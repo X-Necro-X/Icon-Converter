3d6651348a149af73451baed45354588a0164311
Necro Mancer <necromancer@users.noreply.github.com>
2021-02-06 02:28:31 +0530

    waiting for exe

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 import PIL.Image
 try:
     path = input('Enter the path of image: ')
+    save = input('Save location: ')
     icon = PIL.Image.open(path)
     width, height = icon.size
     crop = width if width <= height else height
     icon = icon.crop(((width - crop) // 2, (height - crop) // 2, (width + crop) // 2, (height + crop) // 2)).resize((256,256))
-    icon.save('icon.ico', format = 'ICO', sizes=[(256,256)], quality=95)
+    icon.save(save+'/icon.ico', format = 'ICO', sizes=[(256,256)], quality=95)
     print('Done!')
 except:
     print('Error!')
